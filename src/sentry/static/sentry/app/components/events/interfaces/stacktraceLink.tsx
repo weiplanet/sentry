8ff86851f91cba6e767a1c21b62a2dc66e56a43f
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2021-02-04 11:16:04 -0800

    rm todo

--- src/sentry/static/sentry/app/components/events/interfaces/stacktraceLink.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/stacktraceLink.tsx
@@ -81,7 +81,7 @@ class StacktraceLink extends AsyncComponent<Props, State> {
               </span>
             </ExternalLink>
           ),
-        }); // TODO Nisanthan: add the icon that goes to the docs.
+        });
       default:
         return t('There was an error encountered with the code mapping for this project');
     }

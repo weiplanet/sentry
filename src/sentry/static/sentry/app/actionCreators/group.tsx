e5b821356b8ef5abea8b801d2ef5f8ead8305287
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-02-05 21:45:41 -0800

    ref(js): Move bulk actions out of the api client

--- src/sentry/static/sentry/app/actionCreators/group.tsx
+++ src/sentry/static/sentry/app/actionCreators/group.tsx
@@ -1,7 +1,9 @@
 import * as Sentry from '@sentry/react';
+import isNil from 'lodash/isNil';
+import isUndefined from 'lodash/isUndefined';
 
 import GroupActions from 'app/actions/groupActions';
-import {Client} from 'app/api';
+import {Client, RequestCallbacks, RequestOptions} from 'app/api';
 import GroupStore from 'app/stores/groupStore';
 import {Actor, Group, Member, Note, User} from 'app/types';
 import {buildTeamId, buildUserId} from 'app/utils';
@@ -170,4 +172,200 @@ export function updateNote(
   promise.catch(() => GroupStore.updateActivity(group.id, id, {text: oldText}));
 
   return promise;
+}
+
+type ParamsType = {
+  itemIds?: Array<number> | Array<string>;
+  query?: string;
+  environment?: string | Array<string> | null;
+  project?: Array<number> | null;
+};
+
+type QueryArgs =
+  | {
+      query: string;
+      environment?: string | Array<string>;
+      project?: Array<number>;
+    }
+  | {
+      id: Array<number> | Array<string>;
+      environment?: string | Array<string>;
+      project?: Array<number>;
+    }
+  | {
+      environment?: string | Array<string>;
+      project?: Array<number>;
+    };
+
+/**
+ * Converts input parameters to API-compatible query arguments
+ */
+export function paramsToQueryArgs(params: ParamsType): QueryArgs {
+  const p: QueryArgs = params.itemIds
+    ? {id: params.itemIds} // items matching array of itemids
+    : params.query
+    ? {query: params.query} // items matching search query
+    : {}; // all items
+
+  // only include environment if it is not null/undefined
+  if (params.query && !isNil(params.environment)) {
+    p.environment = params.environment;
+  }
+
+  // only include projects if it is not null/undefined/an empty array
+  if (params.project && params.project.length) {
+    p.project = params.project;
+  }
+
+  // only include date filters if they are not null/undefined
+  if (params.query) {
+    ['start', 'end', 'period', 'utc'].forEach(prop => {
+      if (!isNil(params[prop])) {
+        p[prop === 'period' ? 'statsPeriod' : prop] = params[prop];
+      }
+    });
+  }
+  return p;
+}
+
+function chainUtil<Args extends any[]>(
+  ...funcs: Array<((...args: Args) => any) | undefined>
+) {
+  const filteredFuncs = funcs.filter(
+    (f): f is (...args: Args) => any => typeof f === 'function'
+  );
+  return (...args: Args): void => {
+    filteredFuncs.forEach(func => {
+      func.apply(funcs, args);
+    });
+  };
+}
+
+function wrapRequest(
+  api: Client,
+  path: string,
+  options: RequestOptions,
+  extraParams: RequestCallbacks
+) {
+  if (isUndefined(extraParams)) {
+    extraParams = {};
+  }
+
+  options.success = chainUtil(options.success, extraParams.success);
+  options.error = chainUtil(options.error, extraParams.error);
+  options.complete = chainUtil(options.complete, extraParams.complete);
+
+  return api.request(path, options);
+}
+
+type BulkDeleteParams = ParamsType & {
+  orgId: string;
+  projectId?: string;
+};
+
+export function bulkDelete(
+  api: Client,
+  params: BulkDeleteParams,
+  options: RequestCallbacks
+) {
+  const path: string = params.projectId
+    ? `/projects/${params.orgId}/${params.projectId}/issues/`
+    : `/organizations/${params.orgId}/issues/`;
+
+  const query: QueryArgs = paramsToQueryArgs(params);
+  const id: string = uniqueId();
+
+  GroupActions.delete(id, params.itemIds);
+
+  return wrapRequest(
+    api,
+    path,
+    {
+      query,
+      method: 'DELETE',
+      success: response => {
+        GroupActions.deleteSuccess(id, params.itemIds, response);
+      },
+      error: error => {
+        GroupActions.deleteError(id, params.itemIds, error);
+      },
+    },
+    options
+  );
+}
+
+type BulkUpdateParams = ParamsType & {
+  orgId: string;
+  projectId?: string;
+  failSilently?: boolean;
+  data?: any;
+};
+
+export function bulkUpdate(
+  api: Client,
+  params: BulkUpdateParams,
+  options: RequestCallbacks
+) {
+  const path: string = params.projectId
+    ? `/projects/${params.orgId}/${params.projectId}/issues/`
+    : `/organizations/${params.orgId}/issues/`;
+
+  const query: QueryArgs = paramsToQueryArgs(params);
+  const id: string = uniqueId();
+
+  GroupActions.update(id, params.itemIds, params.data);
+
+  return wrapRequest(
+    api,
+    path,
+    {
+      query,
+      method: 'PUT',
+      data: params.data,
+      success: response => {
+        GroupActions.updateSuccess(id, params.itemIds, response);
+      },
+      error: error => {
+        GroupActions.updateError(id, params.itemIds, error, params.failSilently);
+      },
+    },
+    options
+  );
+}
+
+type MergeGroupsParams = ParamsType & {
+  orgId: string;
+  projectId?: string;
+};
+
+export function mergeGroups(
+  api: Client,
+  params: MergeGroupsParams,
+  options: RequestCallbacks
+) {
+  const path: string = params.projectId
+    ? `/projects/${params.orgId}/${params.projectId}/issues/`
+    : `/organizations/${params.orgId}/issues/`;
+
+  const query: QueryArgs = paramsToQueryArgs(params);
+  const id: string = uniqueId();
+
+  GroupActions.merge(id, params.itemIds);
+
+  return wrapRequest(
+    api,
+    path,
+    {
+      query,
+      method: 'PUT',
+      data: {merge: 1},
+      success: response => {
+        GroupActions.mergeSuccess(id, params.itemIds, response);
+      },
+      error: error => {
+        GroupActions.mergeError(id, params.itemIds, error);
+      },
+    },
+    options
+  );
 }
